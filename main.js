93b90eae7925217c679f675d04499cac126603ee
Aaron Woo <awoo@chapman.edu>
2021-09-16 15:07:04 -0700

    Made one conditional for turning

--- main.js
+++ main.js
@@ -6,49 +6,17 @@ document.addEventListener('keydown', onArrowKeyClicks);
 var $car = document.querySelector('.car');
 
 function onArrowKeyClicks(event) {
-  if (data.direction === 'east') {
-    if (event.code === 'ArrowUp') {
-      $car.className = 'car face-north';
-      data.direction = 'north';
-    } else if (event.code === 'ArrowDown') {
-      $car.className = 'car face-south';
-      data.direction = 'south';
-    } else if (event.code === 'ArrowLeft') {
-      $car.className = 'car face-west';
-      data.direction = 'west';
-    }
-  } else if (data.direction === 'north') {
-    if (event.code === 'ArrowDown') {
-      $car.className = 'car face-south';
-      data.direction = 'south';
-    } else if (event.code === 'ArrowLeft') {
-      $car.className = 'car face-west';
-      data.direction = 'west';
-    } else if (event.code === 'ArrowRight') {
-      $car.className = 'car face-east';
-      data.direction = 'east';
-    }
-  } else if (data.direction === 'south') {
-    if (event.code === 'ArrowUp') {
-      $car.className = 'car face-north';
-      data.direction = 'north';
-    } else if (event.code === 'ArrowLeft') {
-      $car.className = 'car face-west';
-      data.direction = 'west';
-    } else if (event.code === 'ArrowRight') {
-      $car.className = 'car face-east';
-      data.direction = 'east';
-    }
-  } else if (data.direction === 'west') {
-    if (event.code === 'ArrowUp') {
-      $car.className = 'car face-north';
-      data.direction = 'north';
-    } else if (event.code === 'ArrowDown') {
-      $car.className = 'car face-south';
-      data.direction = 'south';
-    } else if (event.code === 'ArrowRight') {
-      $car.className = 'car face-east';
-      data.direction = 'east';
-    }
+  if (event.code === 'ArrowUp') {
+    $car.className = 'car face-north';
+    data.direction = 'north';
+  } else if (event.code === 'ArrowDown') {
+    $car.className = 'car face-south';
+    data.direction = 'south';
+  } else if (event.code === 'ArrowRight') {
+    $car.className = 'car face-east';
+    data.direction = 'east';
+  } else {
+    $car.className = 'car face-west';
+    data.direction = 'west';
   }
 }
