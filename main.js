355cda9c02879e47b28fcd6cafcb94594ae26953
Aaron Woo <awoo@chapman.edu>
2021-09-16 15:46:03 -0700

    Completed issue-3-start-car.

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+
+var $car = document.querySelector('.car');
+
 var data = {
-  direction: 'east'
+  direction: 'east',
+  location: {
+    x: $car.style.left,
+    y: $car.style.top
+  }
 };
 
 document.addEventListener('keydown', onArrowKeyClicks);
-var $car = document.querySelector('.car');
 
 function onArrowKeyClicks(event) {
   if (event.code === 'ArrowUp') {
@@ -15,8 +21,25 @@ function onArrowKeyClicks(event) {
   } else if (event.code === 'ArrowRight') {
     $car.className = 'car face-east';
     data.direction = 'east';
-  } else {
+  } else if (event.code === 'ArrowLeft') {
     $car.className = 'car face-west';
     data.direction = 'west';
   }
 }
+
+function moveCarRight() {
+  var leftVal = parseInt($car.style.left);
+  leftVal += 5;
+  $car.style.left = leftVal + 'px';
+  data.location.x = $car.style.left;
+  data.location.y = $car.sytle.top;
+}
+
+function startCar(event) {
+  if (event.code !== 'Space') {
+    return;
+  }
+  setInterval(moveCarRight, 16);
+}
+
+document.addEventListener('keydown', startCar);
