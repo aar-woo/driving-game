2389845d068c0dee59fa532e94a5ad3fa74a9938
Aaron Woo <awoo@chapman.edu>
2021-12-16 16:40:57 -1000

    Calculated whole area of obstacle and logged when the car was within it's x coordinate range of an obstacle.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var data = {
     y: $car.style.top
   },
   carStarted: false,
-  obstacleLocations: {}
+  obstacleLocations: []
 };
 
 document.addEventListener('keydown', onKeyClicks);
@@ -42,33 +42,46 @@ function driveCar() {
   if (data.direction === 'east' || data.direction === 'west') {
     var leftVal = parseInt($car.style.left);
     if (data.direction === 'east') {
-      leftVal += 3;
+      leftVal += 1;
     } else {
-      leftVal -= 3;
+      leftVal -= 1;
     }
     $car.style.left = leftVal + 'px';
   } else {
     var topVal = parseInt($car.style.top);
     if (data.direction === 'south') {
-      topVal += 3;
+      topVal += 1;
     } else {
-      topVal -= 3;
+      topVal -= 1;
     }
     $car.style.top = topVal + 'px';
 
   }
   data.location.x = $car.style.left;
   data.location.y = $car.style.top;
+
+  const carRectLocation = $car.getBoundingClientRect();
+
+  for (let i = 0; i < data.obstacleLocations.length; i++) {
+    if (carRectLocation.x > data.obstacleLocations[i].xStart && carRectLocation.x < data.obstacleLocations[i].xEnd) {
+      // console.log('car crash');
+    }
+  }
 }
 
 const $obstacles = document.querySelectorAll('.stump');
 
 function getObstacleLocations() {
-  const obstacleLocations = {};
   for (let i = 0; i < $obstacles.length; i++) {
-    obstacleLocations[i] = $obstacles[i].getBoundingClientRect();
+    const obstacleData = $obstacles[i].getBoundingClientRect();
+    const obstacleSpace = {
+      xStart: obstacleData.x,
+      xEnd: obstacleData.x + obstacleData.width,
+      yStart: obstacleData.y,
+      yEnd: obstacleData.y - obstacleData.height
+    };
+    data.obstacleLocations.push(obstacleSpace);
   }
-  data.obstacleLocations = obstacleLocations;
 }
 
-window.addEventListener('onload', getObstacleLocations);
+window.addEventListener('load', getObstacleLocations);
